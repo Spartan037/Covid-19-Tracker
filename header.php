b8697e9b864effca5ce4b777b788a87fae4c2ce0
Spartan037 <68962530+Spartan037@users.noreply.github.com>
2021-06-09 23:00:36 +0530

    Update header.php

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
                     <a class="nav-link" href="./blogs.php">Blogs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./about.php">About Me</a>
+                    <a class="nav-link" href="./about.php">About Us</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
